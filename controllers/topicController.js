c5ea9ec6d2db10e93d96658ab6f6081d0cc830d5
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-14 17:01:33 -0500

    fetch, delete, get, update module.export

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1,1 +1,38 @@
-// topicController.js
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+
+var topic = require("../models/topic");
+
+module.exports = {
+    fetch: function (cb) {
+        scrape(function (data) {
+            var articles = data;
+            for (let i = 0; i < articles.length; i++) {
+                articles[i].date = makeDate();
+                articles[i].saved = false;
+            }
+
+            topic.collection.insertMany(articles, { ordered: false }, function (err, docs) {
+                cb(err, docs);
+            });
+        });
+    },
+    delete: function (query, cb) {
+        topic.remove(query, cb);
+    },
+    get: function (query, cb) {
+        topic.find(query)
+            .sort({
+                _id: -1
+            })
+            .exec(function (err, doc) {
+                cb(doc);
+            });
+    },
+    update: function (query, cb) {
+        topic.update({ _id: query._id }, {
+            $set: query
+        }, {}, cb);
+    }
+}
