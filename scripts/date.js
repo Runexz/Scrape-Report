f2778c784cd75ae6f8bca403341c7c7c58c3d3ac
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-14 16:15:51 -0500

    made date function and exported it

--- scripts/date.js
+++ scripts/date.js
@@ -1,1 +1,12 @@
-// date.js
+var makeDate = function() {
+    var d = new Date();
+    var formattedDate = "";
+
+    formattedDate += (d.getMonth() + 1) + "_";
+    formattedDate += d.getDate() + "_";
+    formattedDate += d.getFullYear();
+
+    return formattedDate;
+};
+
+module.exports = makeDate;
