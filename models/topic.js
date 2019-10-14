55397f28aba9f3e50514942a250edca3bdc95f6d
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-14 14:22:10 -0500

    topic.js schema created

--- models/topic.js
+++ models/topic.js
@@ -1,1 +1,27 @@
-// topic.js
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+var topicSchema = new Schema({
+    topic: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    date: String,
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Topic = mongoose.model("Topic", topicSchema);
+
+// Export the Topic model
+module.exports = Topic;
