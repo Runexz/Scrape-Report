8ec9ba503a2e571a5d4988f9417877c93a92334e
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-14 13:25:45 -0500

    export the routes / and /saved for server file

--- config/routes.js
+++ config/routes.js
@@ -1,1 +1,11 @@
-// routes.js
+// Include routes in server file
+module.exports = function(router) {
+    // Route renders the home handlebars page
+    router.get("/", function(req, res) {
+        res.render("home");
+    });
+    // Route renders the saved handlebars page
+    router.get("/saved", function(req, res) {
+        res.render("saved");
+    });
+}
