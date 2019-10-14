9ecc91f8e0955edc60053a630bfe32b5260f78cc
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-14 14:26:51 -0500

    created schema for note

--- models/note.js
+++ models/note.js
@@ -1,1 +1,19 @@
-// note.js
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+var noteSchema = new Schema({
+    _topicId: {
+        type: Schema.Types.ObjectId,
+        ref: "Topic"
+    },
+    date: String,
+    noteText: String
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Note = mongoose.model("Note", topicSchema);
+
+// Export the Topic model
+module.exports = Note;
